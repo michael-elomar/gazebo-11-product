c175b2bda616eb3831083999b9b2dddba0eed3a5
Michael El Omar <michaelelomar@outlook.com>
2026-04-05 21:39:42 +0200

    add jetty variant

---
+++ jetty/env/dependencies.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env sh
+
+sudo apt install \
+	libprotoc-dev \
+    libprotobuf-dev \
+	libboost-all-dev \
+	libcrypto++-dev \
+	libproj-dev \
+	graphviz \
+	libcppunit-dev \
+	libxaw7-dev \
+	rapidjson-dev \
+	libavdevice-dev \
+	libavfilter-dev \
+	libpostproc-dev \
+	libogre-next-dev \
+	libogre-1.9-dev
